bd75a8ee9451fcdd11b84072003d353f54c6753a
alex-home <361934718@qq.com>
2018-08-26 23:26:26 +0800

    完成基本的页面布局

--- src/view/task/import-task.vue
+++ src/view/task/import-task.vue
@@ -38,36 +38,63 @@
 
         <Col span="12">
           <FormItem label="数据分类">
-            <Select v-model="sourceInfo.category">
-              <Option v-for="item in sourceInfo.category" :value="item">{{item}}</Option>
+            <Select v-model="importSetting.category">
+              <Option v-for="(_, item) in sourceInfo.category" :value="item">{{item}}</Option>
             </Select>
           </FormItem>
         </Col>
 
         <Col span="12">
           <FormItem label="子分类(tag)">
-            <Select v-model="sourceInfo.category">
-              <Option v-for="item in sourceInfo.category" :value="item">{{item}}</Option>
+            <Select v-model="importSetting.subcategory">
+              <Option v-for="(_, item) in sourceInfo.subcategory" :value="item">{{item}}</Option>
             </Select>
           </FormItem>
         </Col>
       </Row>
 
-      <Row :gutter="24">
+      <Row :gutter="12">
         <Col span="12">
-          <FormItem label="人脸检测" label-position="right">
-            <Select v-model="sourceInfo.category">
-              <Option value="item">无</Option>
+          <FormItem label="视频抽帧" label-position="right">
+            <Select v-model="importSetting.extract.method" @on-change="onSelectExtract()">
+              <Option v-for="(item, _) in sourceInfo.extract" :value="item.value">{{item.title}}</Option>
             </Select>
           </FormItem>
-        </Col>
 
+          <Col v-if="importSetting.hasExtract">
+            <FormItem label="抽帧间隔">
+              <Input v-model="importSetting.extract.value" placeholder="设置抽帧时间间隔，单位毫秒"></Input>
+            </FormItem>
+          </Col>
+        </Col>
         <Col span="12">
-          <FormItem label="视频抽帧">
-            <Select v-model="sourceInfo.category">
-              <Option value="item">无</Option>
+          <FormItem label="人脸检测">
+            <Select v-model="importSetting.facedetect.method" @on-change="onSelectFaceDet()">
+              <Option v-for="(item, _) in sourceInfo.facedetect" :value="item.value">{{item.title}}</Option>
             </Select>
           </FormItem>
+
+          <Col v-if="importSetting.hasFaceDetect">
+            <FormItem label="置信度">
+              <InputNumber :max="1" :min="0.1" :step="0.01" size="large" v-model="importSetting.facedetect.confidence" placeholder="人脸检测置信度" style="width:100%"></InputNumber>
+            </FormItem>
+
+            <FormItem label="人脸尺寸">
+              <Row>
+                <Col span="11" style="text-align: left">
+                  <InputNumber :min="1" :step="1" placeholder="高度" v-model="importSetting.facedetect.size.height" style="width:100%"></InputNumber>
+                </Col>
+                <Col span="2" style="text-align: center">--</Col>
+                <Col span="11" style="text-align: left">
+                  <InputNumber  :min="1" :step="1" placeholder="宽度" v-model="importSetting.facedetect.size.width" style="width:100%"></InputNumber>
+                </Col>
+              </Row>
+            </FormItem>
+
+            <FormItem>
+              <Checkbox v-model="importSetting.facedetect.crop">是否进行人脸抠图</Checkbox>
+            </FormItem>
+          </Col>
         </Col>
       </Row>
 
@@ -83,70 +110,90 @@
 </template>
 
 <script>
-import {getImportSourceInfo} from '@/api/task'
+  import {getImportSourceInfo} from '@/api/task'
+  import CollapsedMenu from "../main/components/side-menu/collapsed-menu";
 
-export default {
-  name: 'data-import-task',
-  components: {},
-  data () {
-    return {
-      sourceInfo: {
-        'account': ['pushenglin' ],
-        'source': ['fudu', 'subway'],
-        'category': {
-          'fruit': {
-            'apple': 100,
-            'peach': 1001
+  export default {
+    name: 'data-import-task',
+    components: {CollapsedMenu},
+    data() {
+      return {
+        sourceInfo: {
+          'account': ['pushenglin'],
+          'source': ['fudu', 'subway'],
+          'category': {
+            'fruit': {
+              'apple': 100,
+              'peach': 1001
+            },
+            'subway': {
+              'elevator-head': 1000,
+              'failover': 10001
+            }
           },
-          'subway': {
-            'elevator-head': 1000,
-            'failover': 10001
-          }
+          'extract': [
+            {
+              'title': '无',
+              'value': 0
+            },
+            {
+              'title': '按时间间隔',
+              'value': 'fix-time'
+            }
+          ],
+          'facedetect': [
+            {
+              'title': '无',
+              'value': 0
+            },
+            {
+              'title': 'SSD',
+              'value': 'ssd'
+            }
+          ]
         },
-        'extract': [
-          {
-            'title': '按时间间隔',
-            'item': 'fix-time'
+        importSetting: {
+          account: null,
+          title: null,
+          data_path: null,
+          desc: '',
+          hasExtract: false,
+          hasFaceDetect: false,
+          source: {
+            source: null,
+            category: null,
+            subcategory: null
+          },
+          extract: {
+            method: 0,
+            value: 1000
+          },
+          facedetect: {
+            method: 0,
+            confidence: 0.99,
+            size: {
+              height: 1,
+              width: 1
+            },
+            crop: false
           }
-        ],
-        'facedetect': [
-          {
-            'title': '按时间间隔',
-            'item': 'fix-time'
-          }
-        ]
-      },
-      importSetting: {
-        account: null,
-        title: null,
-        data_path: null,
-        desc: '',
-        hasExtract: false,
-        hasFaceDetect: false,
-        source: {
-          source: null,
-          category: null,
-          subcategory: null
-        },
-        extract: {
-          method: null,
-          value: null
-        },
-        facedetect: {
-          method: null,
-          confidence: null,
-          size: [],
-          crop: false
         }
       }
+    },
+    methods: {
+      onSelectFaceDet: function () {
+        this.importSetting.hasFaceDetect = !!this.importSetting.facedetect.method;
+      },
+      onSelectExtract: function () {
+        this.importSetting.hasExtract = !!this.importSetting.extract.method;
+      }
+    },
+    mounted: function () {
+      // getImportSourceInfo().then(res => {
+      //   this.sourceInfo = res.data
+      // }).catch(err => {
+      //   console.log(err)
+      // })
     }
-  },
-  mounted: function () {
-    // getImportSourceInfo().then(res => {
-    //   this.sourceInfo = res.data
-    // }).catch(err => {
-    //   console.log(err)
-    // })
   }
-}
 </script>
